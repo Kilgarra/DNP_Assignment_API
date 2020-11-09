52be772b0ccbc93f9b093824208269b0555ce189
Marton Pentek <pentek.marci@gmail.com>
2020-11-09 16:08:52 +0100

    initial

---
+++ DNP_Assignment_API/Controllers/Controller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DNP_Assignment_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Controller : ControllerBase
+    {
+
+    }
+}
